0c5cbf74b91f112dc1a8937b853b7a45f7e44388
Harun <harunf098@gmail.com>
2024-08-12 16:38:50 +0100

    slow burn

--- part2/part2-notes-frontend/src/App.jsx
+++ part2/part2-notes-frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import Note from "./components/Note";
 import { useState, useEffect } from "react";
-import axios from "axios";
 import noteService from "./services/notes";
 // import "./index.css";
 
